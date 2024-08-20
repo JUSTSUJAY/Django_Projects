7681da9653a515be52872186d57752bb780b4087
JUSTSUJAY <m2010070@spce.ac.in>
2024-08-20 16:13:41 +0530

    WallPapers Gallery

---
+++ 5 Wallpaper Gallery/wallpapers/gallery/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Product
+
+# Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product_list.html', {'products':products})
+    
